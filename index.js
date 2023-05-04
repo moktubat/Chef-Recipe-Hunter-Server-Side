a303f6c3938795b619f0f1a80cceb088fdf9790a
moktubat <shadhinreja@gmail.com>
2023-05-04 23:46:48 +0600

    json added

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 const app = express();
 
 const chefs = require("./chefs.json");
+
+const chefInfo = require("./chefInfo.json");
+
 app.use(cors())
 app.get('/', (req, res) =>{
     res.json({mess: "hi"});
@@ -10,6 +13,16 @@ app.get('/', (req, res) =>{
 
 app.get('/chefs', (req, res) =>{
     res.json(chefs);
+})
+
+app.get('/chefInfo', (req, res) => {
+    res.send(chefInfo);
+})
+
+app.get('/chefInfo/:id', (req, res) => {
+    const id = req.params.id;
+    const singleChef = chefInfo.find(item => item.id === id);
+    res.send(singleChef);
 })
 
 app.listen(5000, () =>{
